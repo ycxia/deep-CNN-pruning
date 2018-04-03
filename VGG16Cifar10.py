5973ef5a0a4c1fc3c2050157382ad260fe33cd06
ThenAndNoThen <w86568376@163.com>
2018-04-03 11:32:36 +0800

    add dropout

--- VGG16Cifar10.py
+++ VGG16Cifar10.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-import itertools
+
 class VGG16Cifar10:
     def __init__(self):
         self.x = tf.placeholder(tf.float32, shape=(None, 32, 32, 3))
@@ -7,20 +7,6 @@ class VGG16Cifar10:
         self.filters = []
         self.dense = []
         self.bais = []
-
-        # self.filters.append(tf.Variable(np.random.rand(3, 3, 3, 64), dtype=np.float32))
-        # self.filters.append(tf.Variable(np.random.rand(3, 3, 64, 64), dtype=np.float32))
-        # self.filters.append(tf.Variable(np.random.rand(3, 3, 64, 128), dtype=np.float32))
-        # self.filters.append(tf.Variable(np.random.rand(3, 3, 128, 128), dtype=np.float32))
-        # self.filters.append(tf.Variable(np.random.rand(3, 3, 128, 256), dtype=np.float32))
-        # self.filters.append(tf.Variable(np.random.rand(3, 3, 256, 256), dtype=np.float32))
-        # self.filters.append(tf.Variable(np.random.rand(3, 3, 256, 256), dtype=np.float32))
-        # self.filters.append(tf.Variable(np.random.rand(3, 3, 256, 512), dtype=np.float32))
-        # self.filters.append(tf.Variable(np.random.rand(3, 3, 512, 512), dtype=np.float32))
-        # self.filters.append(tf.Variable(np.random.rand(3, 3, 512, 512), dtype=np.float32))
-        # self.filters.append(tf.Variable(np.random.rand(3, 3, 512, 512), dtype=np.float32))
-        # self.filters.append(tf.Variable(np.random.rand(3, 3, 512, 512), dtype=np.float32))
-        # self.filters.append(tf.Variable(np.random.rand(3, 3, 512, 512), dtype=np.float32))
         self.filters.append(self.get_variable("filter1",shape=[3,3,3,64]))
         self.filters.append(self.get_variable("filter2",shape=[3,3,64,64]))
         self.filters.append(self.get_variable("filter3",shape=[3, 3, 64, 128]))
@@ -40,37 +26,47 @@ class VGG16Cifar10:
         self.dense.append(self.get_variable("dense2",shape=[512,10]))
         self.bais.append(tf.zeros(name="b2", shape=[1, 10]))
 
+        self.regularizer = tf.contrib.layers.l2_regularizer(0.0005)
+
     def build_model(self):
         self.output1 = self.conv2d_with_relu(self.x, self.filters[0])
+        self.output1 = tf.layers.dropout(self.output1,0.3)
         self.output2 = self.conv2d_with_relu(self.output1, self.filters[1])
         polled = tf.nn.max_pool(self.output2, [1,2,2,1], [1,2,2,1],'VALID')
+
         self.output3 = self.conv2d_with_relu(polled, self.filters[2])
+        self.output3 = tf.layers.dropout(self.output3, 0.4)
         self.output4 = self.conv2d_with_relu(self.output3, self.filters[3])
         polled = tf.nn.max_pool(self.output4, [1,2,2,1], [1,2,2,1], 'VALID')
+
         self.output5 = self.conv2d_with_relu(polled, self.filters[4])
+        self.output5 = tf.layers.dropout(self.output5, 0.4)
         self.output6 = self.conv2d_with_relu(self.output5, self.filters[5])
+        self.output6 = tf.layers.dropout(self.output6, 0.4)
         self.output7 = self.conv2d_with_relu(self.output6, self.filters[6])
         polled = tf.nn.max_pool(self.output7, [1,2,2,1], [1, 2, 2, 1], 'VALID')
+
         self.output8 = self.conv2d_with_relu(polled, self.filters[7])
+        self.output8 = tf.layers.dropout(self.output8, 0.4)
         self.output9 = self.conv2d_with_relu(self.output8, self.filters[8])
+        self.output9 = tf.layers.dropout(self.output9, 0.4)
         self.output10 = self.conv2d_with_relu(self.output9, self.filters[9])
         polled = tf.nn.max_pool(self.output10, [1,2,2,1], [1, 2, 2, 1], 'VALID')
+
         self.output11 = self.conv2d_with_relu(polled, self.filters[10])
+        self.output11 = tf.layers.dropout(self.output11, 0.4)
         self.output12 = self.conv2d_with_relu(self.output11, self.filters[11])
+        self.output12 = tf.layers.dropout(self.output12, 0.4)
         self.output13 = self.conv2d_with_relu(self.output12, self.filters[12])
         polled = tf.nn.max_pool(self.output13, [1,2,2,1], [1, 2, 2, 1], 'VALID')
 
         polled = tf.reshape(polled,[-1,512])
+        polled = tf.layers.dropout(polled, 0.4)
         fc1 = self.fc(polled,self.dense[0],self.bais[0])
-        fc1 = tf.nn.relu(fc1)
         fc1 = tf.layers.batch_normalization(fc1)
-        # fc1 = tf.layers.dense(polled,512,tf.nn.relu)
+        fc1 = tf.nn.relu(fc1)
         fc1 = tf.nn.dropout(fc1,0.5)
-
         self.y = self.fc(fc1,self.dense[1],self.bais[1])
-        # self.y = tf.layers.dense(fc1,10)
-        # self.fc1 = self.fc_relu_drop(polled)
-        # self.y = tf.layers.dense(inputs=self.fc1, units=10, kernel_initializer=tf.truncated_normal_initializer(mean=0, stddev=1))
         self.yy = tf.nn.softmax(self.y)
         correct_prediction = tf.equal(tf.argmax(self.yy,1), tf.argmax(self.y_,1))
         self.accaury = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
@@ -79,12 +75,10 @@ class VGG16Cifar10:
             labels=self.y_,
             logits=self.y))
 
-        # self.add_weight_to_collection()
-
     def conv2d_with_relu(self, input, filter):
         output = tf.nn.conv2d(input, filter, [1, 1, 1, 1], 'SAME')
-        output = tf.nn.relu(output)
         output = tf.layers.batch_normalization(output)
+        output = tf.nn.relu(output)
         return output
 
     def fc(self,input,dense,bais):
@@ -92,14 +86,13 @@ class VGG16Cifar10:
 
     def get_train_step(self, learning_rate,lbd):
         # regularizer = tf.contrib.layers.l2_regularizer(scale=lbd)
-        # self.reg_term = tf.contrib.layers.apply_regularization(regularizer)
-        tv = tf.trainable_variables()
-        print("all weight shape:{}".format(len(tv)))
-        regularization_cost = 0.0005 * tf.reduce_sum([tf.nn.l2_loss(v) for v in itertools.chain(self.filters,self.dense)])
-        return tf.train.AdamOptimizer(learning_rate).minimize(self.cross_entropy+regularization_cost)
+        # reg_term = tf.reduce_sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
+        # regularization_cost = 0.0005 * tf.reduce_sum([tf.nn.l2_loss(v) for v in itertools.chain(self.filters,self.dense)])
+        return tf.train.AdamOptimizer(learning_rate).minimize(self.cross_entropy)
 
     def get_variable(self,name,shape):
-        return tf.get_variable(name=name,shape=shape,initializer=tf.glorot_normal_initializer())
+        return tf.get_variable(name=name,shape=shape,initializer=tf.glorot_normal_initializer()
+                               ,regularizer = self.regularizer)
 
     def add_weight_to_collection(self):
         for filter in self.filters:
