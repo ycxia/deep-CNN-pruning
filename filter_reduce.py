d5b2872257c6146ea0a98a2b0518b0e95b5db120
ThenAndNoThen <w86568376@163.com>
2018-05-02 18:03:56 +0800

    通用剪枝函数

--- filter_reduce.py
+++ filter_reduce.py
@@ -1,15 +1,41 @@
 import tensorflow as tf
 
 #去除输出第index个通道的相关卷积核
-def reduceFilterOutput(input, index):
+def reduceFilterOutput(input, index, sess, realLen):
     shape = input.shape
     left = tf.slice(input, [0,0,0,0], [shape[0],shape[1],shape[2],index])
-    right = tf.slice(input, [0,0,0,index+1], [shape[0],shape[1],shape[2],shape[3]-index-1])
-    return tf.concat([left, right], 3)
+    right = tf.slice(input, [0,0,0,index+1], [shape[0],shape[1],shape[2],realLen-index-1])
+    ret = tf.concat([left, right], 3)
+    sess.run(tf.assign(input, ret, validate_shape=False))
+    return ret
 
 #去除处理第index个输入通道的相关卷积核
-def reduceFilterInput(input, index):
+def reduceFilterInput(input, index, sess, realLen):
     shape = input.shape
     left = tf.slice(input, [0,0,0,0], [shape[0],shape[1],index,shape[3]])
-    right = tf.slice(input, [0,0,index+1,0], [shape[0],shape[1],shape[2]-index-1,shape[3]])
-    return tf.concat([left, right], 2)
+    right = tf.slice(input, [0,0,index+1,0], [shape[0],shape[1],realLen-index-1,shape[3]])
+    ret = tf.concat([left, right], 2)
+    sess.run(tf.assign(input, ret, validate_shape=False))
+    return ret
+
+def reduceFilter(input, sess, target_index, target_dim, shape):
+    leftSize = []
+    rightSize = []
+    left_start = []
+    rightStart = []
+    for (num,i) in zip(shape,range(len(shape))):
+        if(i==target_dim):
+            leftSize.append(target_index)
+            rightSize.append(num-1-target_index)
+            rightStart.append(target_index+1)
+        else:
+            leftSize.append(num)
+            rightSize.append(num)
+            rightStart.append(0)
+        left_start.append(0)
+    left = tf.slice(input, left_start, leftSize)
+    right = tf.slice(input, rightStart, rightSize)
+    ret = tf.concat([left, right], target_dim)
+    sess.run(tf.assign(input, ret, validate_shape=False))
+    shape[target_dim] = shape[target_dim]-1
+    print(ret)
--- test.py
+++ test.py
@@ -6,12 +6,70 @@ from util import Cifar10Dataset
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
+from ResNet20 import ResNet20
 import tensorflow as tf
-from VGG16SEBlock import VGG16SEBlock
+import filter_reduce as fr
 
-model = VGG16SEBlock(0.0001)
+cifar10 = Cifar10Dataset("/home/wxj/下载/dataset/cifar-10-batches-py")
+cifar10.load_test_data()
+print("Test data load success,test set shape:{}".format(cifar10.test_x.shape))
+model = ResNet20()
 model.build_model()
-model.build_model()
+with tf.variable_scope("",reuse=True):
+    f1 = tf.get_variable("block1/conv2d/kernel")
+    f2 = tf.get_variable("block1/conv2d_1/kernel")
+    bn_gm = tf.get_variable("block1/batch_normalization/gamma")
+    bn_mean = tf.get_variable("block1/batch_normalization/moving_mean")
+    bn_var = tf.get_variable("block1/batch_normalization/moving_variance")
+    bn_beta = tf.get_variable("block1/batch_normalization/beta")
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+acc = sess.run(model.accaury,feed_dict={model.x:cifar10.test_x[:100],model.y_:cifar10.test_label[:100], model.isTrain:False})
+print(acc)
+shape1 = list((int(f1.shape[i]) for i in range(4)))
+shape2 = list((int(f2.shape[i]) for i in range(4)))
+fr.reduceFilter(f1, sess, 0, 3, shape1)
+fr.reduceFilter(f2, sess, 0, 2, shape2)
+fr.reduceFilter(f1, sess, 0, 3, shape1)
+fr.reduceFilter(f2, sess, 0, 2, shape2)
+fr.reduceFilter(f1, sess, 0, 3, shape1)
+fr.reduceFilter(f2, sess, 0, 2, shape2)
+fr.reduceFilter(f1, sess, 0, 3, shape1)
+fr.reduceFilter(f2, sess, 0, 2, shape2)
+
+fr.reduceFilter(bn_gm, sess, 0, 0, [16])
+fr.reduceFilter(bn_mean, sess, 0, 0, [16])
+fr.reduceFilter(bn_var, sess, 0, 0, [16])
+fr.reduceFilter(bn_beta, sess, 0, 0, [16])
+fr.reduceFilter(bn_gm, sess, 0, 0, [15])
+fr.reduceFilter(bn_mean, sess, 0, 0, [15])
+fr.reduceFilter(bn_var, sess, 0, 0, [15])
+fr.reduceFilter(bn_beta, sess, 0, 0, [15])
+fr.reduceFilter(bn_gm, sess, 0, 0, [14])
+fr.reduceFilter(bn_mean, sess, 0, 0, [14])
+fr.reduceFilter(bn_var, sess, 0, 0, [14])
+fr.reduceFilter(bn_beta, sess, 0, 0, [14])
+fr.reduceFilter(bn_gm, sess, 0, 0, [13])
+fr.reduceFilter(bn_mean, sess, 0, 0, [13])
+fr.reduceFilter(bn_var, sess, 0, 0, [13])
+fr.reduceFilter(bn_beta, sess, 0, 0, [13])
+
+
+
+acc = sess.run(model.accaury, feed_dict={model.x: cifar10.test_x[:100], model.y_: cifar10.test_label[:100], model.isTrain:False})
+print(acc)
+sess.close()
+
+
+
+# tensorboard_dir = 'tensorboard/ResNet20'  # 保存目录
+# if not os.path.exists(tensorboard_dir):
+#     os.makedirs(tensorboard_dir)
+# with tf.Session() as sess:
+#     writer = tf.summary.FileWriter(tensorboard_dir)
+#     writer.add_graph(sess.graph)
+
 
 
 
